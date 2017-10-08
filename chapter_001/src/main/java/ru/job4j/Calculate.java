1cc27bee1eec8dbb6fa3f735ce5f5cb6e2e068d8
Andrew A. Poryadin <dron600@gmail.com>
2017-10-08 12:53:56 +0300

    1.3. JavaDoc. Code convention [#4087]

--- chapter_001/src/main/java/ru/job4j/Calculate.java
+++ chapter_001/src/main/java/ru/job4j/Calculate.java
@@ -1,7 +1,18 @@
 package ru.job4j;
 
+/**
+ * Calculate.
+ *
+ * @author Andrew A. Poryadin (dron600@gmail.com)
+ * @version $Id$
+ * @since 0.1
+ */
 public class Calculate {
 
+     /**
+     * Main.
+     * @param args - args.
+     */
     public static void main(String[] args) {
         System.out.println("Hello World");
     }
